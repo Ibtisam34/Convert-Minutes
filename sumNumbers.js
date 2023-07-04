e24cb44c9795d5f8df674ea4e973b62540e701a2
ibtisam34 <riyanagwl@gmail.com>
2023-07-04 13:21:01 +0300

    Add numbers

---
+++ sumNumbers.js
@@ -0,0 +1,8 @@
+function sumNumbers(num1, num2) {
+    return num1 + num2;
+  }
+  // Testing the function
+  const number1 = 5;
+  const number2 = 10;
+  const sum = sumNumbers(number1, number2);
+  console.log(`The sum of ${number1} and ${number2} is ${sum}.`);
